a58deabded3fae9aa4d98d7a2f649b2745ae7768
Wojciech Czok <wojciech.czok@gmail.com>
2026-05-17 00:28:51 +0200

    added basic methods to LibraryService class

--- src/main/java/service/LibraryService.java
+++ src/main/java/service/LibraryService.java
@@ -1,12 +1,12 @@
 package service;
 
-import model.LibraryData;
+import model.*;
 
 /**
  * Klasa odpowiadajaca za logike biznesowa systemu bibliotycznego
  */
 public class LibraryService {
-    //obiekt zwieracacy dane aplikacji
+    //obiekt zwierajacy dane aplikacji
     private final LibraryData data;
 
     // serwis dla przetwarzania danych biblioteki
@@ -17,5 +17,22 @@ public class LibraryService {
 
     public LibraryData getData() {
         return data;
+    }
+    // metoda dodawanie ksiazki
+    public void addBook(Book book){
+        data.getBooks().add(book);
+    }
+
+    // metoda dodawanie departamentu
+    public void addDepartment(Department department){
+        data.getDepartments().add(department);
+    }
+    // metoda dodawanie polki
+    public void addShelf(Shelf shelf){
+        data.getShelves().add(shelf);
+    }
+    // metoda dodawanie czytelnika
+    public void addReader(Reader reader){
+        data.getReaders().add(reader);
     }
 }
